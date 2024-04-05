9f991b818f3dfad05f57f4e90e30badf784e8d4a
Grace Fan <fanxirouani@gmail.com>
2024-04-05 14:03:47 -0700

    added loops notes

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -1,2 +1,26 @@
 #More fuction
 #Author :Grace
+
+
+   # if value == 0 or value == 1 "*"
+    # if value > 1 "*" * num_stars
+    # else negative number -> error :
+    if num_stars == 0 or num_stars == 1:
+        value = "*"
+    elif num_stars > 1:
+        value = "*" * num_stars
+    else:
+        value = "Sorry, can't take negative numbers."
+
+    return value
+
+
+# Multiply Strings
+# greeting = "hello"
+# print(greeting * 2)
+
+# print("The quick brown fox jumps over the lazy dog." * 2)
+print(stars(1))  # "*"
+print(stars(100))  # "*" * 100
+print(stars(0))
+print(stars(-1))
